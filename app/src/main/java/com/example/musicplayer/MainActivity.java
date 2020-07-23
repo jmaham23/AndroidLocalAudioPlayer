2dabd352cbdb406f351d438d6dd4178cf7377e3f
jmaham23 <48227683+jmaham23@users.noreply.github.com>
2020-07-22 21:09:00 -0700

    App now request permissions upon launching

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.app.ActivityManager;
+import android.os.Build.VERSION;
+import java.lang.Object;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private static final int REQUEST_PERMS = 0;
+    private static final int REQUEST_PERMS = 0; //request code
     private static final int PERMS_COUNT = 1;
     //permission to read files like music files from devices storage
     private static final String[] PERMS = {Manifest.permission.READ_EXTERNAL_STORAGE};
@@ -28,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return false;
+    }
+
+    public void onRequestPermissionsResult(int reqCode, String[] perms, int[] grantRes){
+        super.onRequestPermissionsResult(reqCode, perms, grantRes);
+
+
+        if(permsDenied()){
+            //recreate activity if user is denied activity
+            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+            recreate();
+        }
+
+        else{
+            onResume();
+        }
+    }
+
+    //check if we have to request permissions
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(permsDenied()){
+            requestPermissions(PERMS, REQUEST_PERMS);
+            return;
+        }
     }
 }
